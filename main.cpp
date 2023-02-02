13e1e098e7b8f47d3742091154b8d9fdcf30ce5b
k2supra <andrijkorolevic@gmail.com>
2023-02-02 14:42:20 +0200

    .

--- main.cpp
+++ main.cpp
@@ -2,30 +2,40 @@
 #include <ctime>
 using namespace std;
 
-int S_U_M(int arr[], int sum, int ten_size);
+int S_U_M(int arr[], int sum, int ten_size, int arr_sum[], int begin);
 
 int main() {
 	srand(time(0));
 	const int size = 100;
 	int sum = 0;
+	const int sum_size = 100;
+	int counter = 0;
+	int begin = 0;
 	int ten_size = 10;
 	int arr[size]{};
+	int arr_sum[size]{};
 	for (size_t i = 0; i < size; i++)
 	{
 		arr[i] = rand() % 1000;
+		cout << arr[i] << ' ';
 	}
-	S_U_M(arr, sum, ten_size);
+	S_U_M(arr, sum, ten_size, arr_sum, begin);
+	
 }
 
-int S_U_M(int arr[], int sum, int ten_size) {
-	for (size_t i = 0; i < ten_size; i++)
+int S_U_M(int arr[], int sum, int ten_size, int arr_sum[], int begin) {
+	for (begin; begin < ten_size; begin++)
 	{
-		/*cout << "Num=" << arr[i];*/
-		sum += arr[i];
-		/*cout << "Res = " << sum;*/
+		sum += arr[begin];
+		arr_sum[begin] = sum;
+		if (begin == 9)
+		{
+			cout << "\nArr_sum = " << arr_sum[begin];
+			
+		}
 	}
-	cout << "Res = " << sum;
-	/*sum += 15;
-	cout << "hello!" << sum;*/
+	/*arr_sum[0] = sum;*/
+	/*cout << "\nRes = " << sum;*/
+	/*cout << "\nArr_sum = " << arr_sum[0];*/
 	return 0;
 }
